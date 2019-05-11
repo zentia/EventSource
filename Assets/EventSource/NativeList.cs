6a20994d8ad5295ae5e47266bec32cf0e89de19e
zentia <363476658@qq.com>
2019-05-11 09:29:32 +0800

    list

---
+++ Assets/EventSource/NativeList.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+unsafe struct NativeListData
+{
+    public byte* arrayPtr;
+    public int length;
+    public int capacity;
+    public int structSize;
+    public const int SIZE = 20;
+}
+
+public unsafe class NativeList<T>:IDisposable where T : struct
+{
+    private NativeListData* data;
+
+    public int length
+    {
+        get { return data->length; }
+    }
+
+    public int capacity
+    {
+        get { return data->capacity; }
+    }
+
+    public NativeList(int capacity = 10)
+    {
+        if (capacity < 1) capacity = 1;
+        data = (NativeListData*) NativeMemory.Alloc(NativeListData.SIZE, NativeMemory.NativeMemoryType.RawList);
+        data->structSize = Marshal.SizeOf<NativeListData>();
+        data->capacity = capacity;
+        data->length = 0;
+        data->arrayPtr = NativeMemory.Alloc(data->structSize * data->capacity, NativeMemory.NativeMemoryType.RawList);
+    }
+
+    private void Resize()
+    {
+        int newCapacity = data->capacity * 2;
+        byte* newArr = NativeMemory.Alloc(data->structSize * newCapacity, NativeMemory.NativeMemoryType.RawList);
+        NativeMemory.Copy(newArr, data->arrayPtr, data->structSize * data->capacity);
+        NativeMemory.Free(data->arrayPtr);
+        data->arrayPtr = newArr;
+        data->capacity = newCapacity;
+    }
+
+    public void Add(ref T value)
+    {
+        int lastElement = data->length++;
+        if (data->length > data->capacity) Resize();
+        IntPtr intPrt = Marshal.AllocHGlobal(Marshal.SizeOf<T>());
+        Marshal.StructureToPtr(value, intPrt, true);
+
+    }
+
+    public void Dispose()
+    {
+        
+    }
+}
---
+++ Assets/EventSource/Test.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour
+{
+    public Text UnityAlloc;
+
+    public Text NativeAlloc;
+
+    public struct NativeTransform
+    {
+        public Vector3 localPosition;
+        public Vector3 localScale;
+    }
+
+    public UnsafeList<NativeTransform> nativeTransformList;
+
+    public void TestUnityAlloc()
+    {
+        float start = Time.realtimeSinceStartup;
+        using (nativeTransformList = new UnsafeList<NativeTransform>(100))
+        {
+            
+        }
+        UnityAlloc.text = string.Format("耗时：{0}",Time.realtimeSinceStartup - start);
+    }
+
+    public void TestNativeAlloc()
+    {
+        
+    }
+}
